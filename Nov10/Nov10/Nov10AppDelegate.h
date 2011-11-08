34a36ce0ea5263ff3796ea273553861c6af8799f
burnsb43 <burnsb43@gmail.com>
2011-11-08 09:44:08 -0500

    Flip 4 screens w/o Recognizer

---
+++ Nov10/Nov10/Nov10AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov10AppDelegate.h
+//  Nov10
+//
+//  Created by Barbara Burns on 11/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov10AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
